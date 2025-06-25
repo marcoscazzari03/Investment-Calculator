34c5a119eeed506b7d7164a718d521f28124ce53
Marco Scazzari <marco.scazzari@lefonti.com>
2025-06-25 10:38:20 +0200

    Migrating into Modules + related changes in main.ts

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -5,8 +5,6 @@ import { InvesmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })
